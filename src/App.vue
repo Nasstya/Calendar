1e42eb59e67b10d40ca34fdc0bce63176442829d
belichenk0andreyka <belichenko.andreyka22@gmail.com>
2019-07-12 22:38:03 +0300

    display mass width month

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return{
+      x: [],
+    }
+  },
+  mounted(){
+    function getLastDayOfMonth(year, month) {
+      var date = new Date(year, month + 1, 0);
+      return date.getDate();
+    }
+    for (var i = 0; i < 12; i++){
+      this.x.push(i);
+      this.x[i] = [];
+      for ( var j = 1; j <= getLastDayOfMonth(2019, i); j++){
+        this.x[i].push(j);
+      }
+      document.write(this.x[i] + '<br />');
+    }
+  }
+};
+</script>
+
