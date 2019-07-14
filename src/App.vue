65f525eae55b3269e600d37ff1b6a0c24c217c54
belichenk0andreyka <belichenko.andreyka22@gmail.com>
2019-07-14 14:24:47 +0300

    days of month the days of the week are arranged in a row

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <div></div>
+    <div class="pagination">
+      <button @click="prevPage"
+            :disabled="currentPage === 0"
+            >Pref</button> 
+      <p>{{ nameOfOneMonth }}</p>
+      <button @click="nextPage"
+            :disabled="currentPage === 11"
+            >Next</button> 
+    </div>
+    <div class="calendar">
+      <ul>
+        <div v-for="(monthInCalendar, index) in getCalendar" :key = "index" class="month">{{ monthInCalendar }}</div>
+      </ul> 
+    </div>
   </div>
 </template>
 
@@ -9,23 +22,51 @@
 export default {
   data(){
     return{
-      x: [],
+      currentPage: 0,
+      namesOfMonths: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      nameOfOneMonth: 'January'
     }
   },
-  mounted(){
-    function getLastDayOfMonth(year, month) {
-      var date = new Date(year, month + 1, 0);
-      return date.getDate();
+  computed: {
+    getCalendar(){
+      return this.buildCalendar();
     }
-    for (var i = 0; i < 12; i++){
-      this.x.push(i);
-      this.x[i] = [];
-      for ( var j = 1; j <= getLastDayOfMonth(2019, i); j++){
-        this.x[i].push(j);
+    },
+  methods: {
+    prevPage(){
+      this.currentPage--;
+      this.nameOfOneMonth = this.namesOfMonths[this.currentPage];
+    },
+    nextPage(){
+      this.currentPage++;
+      this.nameOfOneMonth = this.namesOfMonths[this.currentPage];
+    },
+
+    getLastDayOfMonth(year, month) {
+      let date = new Date(year, month + 1, 0);
+      return date.getDate();
+    },
+    buildCalendar(){
+      let massOfMonth = [];
+      for (let months = 0; months < 12; months++){
+        massOfMonth.push(months);
+        massOfMonth[months] = [];
+        for ( let daysInMonth = 1; daysInMonth <= this.getLastDayOfMonth(2019, months); daysInMonth++){
+          massOfMonth[months].push(daysInMonth);
+        }
       }
-      document.write(this.x[i] + '<br />');
+      return massOfMonth[this.currentPage]; 
     }
   }
 };
 </script>
+
+<style>
+  .month{
+    list-style-type: none;
+    
+  }
+</style>
+
+
 
