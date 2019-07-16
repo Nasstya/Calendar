83284fb22114215520338ba75da2042a042528e0
belichenk0andreyka <belichenko.andreyka22@gmail.com>
2019-07-16 11:39:26 +0300

    Don't work calendar of other years besides 2019

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
+<!-- поменял год на гетфуллуеиар -->
 <template>
   <div>
     <div class="pagination">
       <button @click="prevPage"
             :disabled="currentPage === 0"
             >Pref</button> 
-      <p>{{ nameOfOneMonth }}</p>
+      <p>{{ currentPage }} {{ nameOfOneMonth }} {{ year }}</p>
       <button @click="nextPage"
-            :disabled="currentPage === 11"
             >Next</button> 
     </div>
 
@@ -26,30 +26,45 @@ export default {
       currentPage: 0,
       namesOfMonths: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       nameOfOneMonth: 'January',
-      nameOfDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+      nameOfDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+      date: new Date(),
+      year: ''
     }
   },
   computed: {
     getCalendar(){
       return this.buildCalendar();
     }
-    },
+  },
+  mounted(){
+    this.year = this.date.getFullYear();
+  },
   methods: {
     prevPage(){
       this.currentPage--;
+      if(this.currentPage % 11 === 0){
+        this.currentPage == 11;
+        this.year--;
+      }
       this.nameOfOneMonth = this.namesOfMonths[this.currentPage];
     },
     nextPage(){
       this.currentPage++;
+      if(this.currentPage % 11 === 0){
+        this.currentPage == 0;
+        this.year++; 
+      }
       this.nameOfOneMonth = this.namesOfMonths[this.currentPage];
     },
-
-    getLastDayOfMonth(year, month) { // нахождение числа последнего дня в месяце
-      let dateDaysInMonth = new Date(year, month + 1, 0);
+    getYear(){
+      this.year = this.date.getFullYear();
+    },
+    getLastDayOfMonth(month) { // нахождение числа последнего дня в месяце
+      let dateDaysInMonth = new Date(this.year, month + 1, 0);
       return dateDaysInMonth.getDate();
     },
     getNumberOfFirstDayInMonth(month){ //нахождение номера первого дня в месяце
-      let dateFirstDayInMonth = new Date(2019, month, 1);
+      let dateFirstDayInMonth = new Date(this.year, month, 1);
       return dateFirstDayInMonth.getDay();
     },
     buildCalendar(){
@@ -57,31 +72,32 @@ export default {
       for (let months = 0; months < 12; months++){
         massOfMonth.push(months);
         massOfMonth[months] = [];
-        for ( let daysInMonth = 1; daysInMonth <= this.getLastDayOfMonth(2019, months); daysInMonth++){
+        for ( let daysInMonth = 1; daysInMonth <= this.getLastDayOfMonth(months); daysInMonth++){
           massOfMonth[months].push(daysInMonth);
         }
         // Заполняем начало каждого месяца числами из прошлого месяца
         if(this.getNumberOfFirstDayInMonth(months) > 0){
-          let t = this.getLastDayOfMonth(2019, months-1) + 1;
+          let t = this.getLastDayOfMonth(months-1) + 1;
           for(let b = 0; b <= this.getNumberOfFirstDayInMonth(months) - 2; b++){
             t--;
             massOfMonth[months].unshift(t)
           }
         }else if(this.getNumberOfFirstDayInMonth(months) === 0){
-          let u = this.getLastDayOfMonth(2019, months-1) + 1;
+          let u = this.getLastDayOfMonth(months-1) + 1;
           for(let nulldays = 0; nulldays <= 5; nulldays++){
             u--;
             massOfMonth[months].unshift(u);
           }
         }
         //Заполняем конец каждого месяца числами из будущего месяца
-        if(this.getNumberOfFirstDayInMonth(months + 1) >= 1){
-          for(let e = 1; e < this.getNumberOfFirstDayInMonth(months + 1) - 1; e++){
-            massOfMonth[months].push(e);
+        if(this.getNumberOfFirstDayInMonth(months + 1) > 1){
+          let i = 0;
+          for(let q = this.getNumberOfFirstDayInMonth(months + 1); q <= 7; q++){
+            i++;
+            massOfMonth[months].push(i);
           }
-        }else if(this.getNumberOfFirstDayInMonth(months + 1) === 0){
-            let e = 1;
-            massOfMonth[months].push(e);
+        } else if(this.getNumberOfFirstDayInMonth(months + 1) === 0){
+          massOfMonth[months].push(1);
         }
       }
 
@@ -119,21 +135,3 @@ export default {
     justify-content: space-between;
   }
 </style>
-
-
-
-
-<!--         if(this.getNumberOfFirstDayInMonth(months) > 0){
-          for(let nulldays = 0; nulldays <= this.getNumberOfFirstDayInMonth(months) - 2; nulldays++){
-            massOfMonth[months].unshift('');
-          }
-        }else if(this.getNumberOfFirstDayInMonth(months) > 1 && this.getNumberOfFirstDayInMonth(months) < 1){
-          let massOfLastDaysOfPastMonth = [];
-          for(let b = this.getLastDayOfMonth(2019, months); b <= this.getNumberOfFirstDayInMonth(months); b--){
-            massOfMonth[months].unshift(b);
-          }
-        }else{
-          for(let nulldays = 0; nulldays <= 5; nulldays++){
-            massOfMonth[months].unshift('');
-          }
-        } -->
